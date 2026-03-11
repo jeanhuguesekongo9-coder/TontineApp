70e705d25bef4e4481b5a890492ba8912c84715a
jeanhuguesekongo9-coder <jeanhuguesekongo9@gmail.com>
2026-03-11 22:54:35 +0000

    fix: clé API dans variable environnement

--- app/emails.py
+++ app/emails.py
@@ -1,17 +1,19 @@
 ﻿# -*- coding: utf-8 -*-
 import requests
+import os
 
 def envoyer_email_verification(utilisateur):
     from app.models import db
-    from flask import url_for, current_app
+    from flask import url_for
     token = utilisateur.generer_token_email()
     db.session.commit()
     lien = url_for('auth.verifier_email', token=token, _external=True)
     
+    api_key = os.environ.get("BREVO_API_KEY", "")
     response = requests.post(
         "https://api.brevo.com/v3/smtp/email",
         headers={
-            "api-key": "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",
+            "api-key": api_key,
             "Content-Type": "application/json"
         },
         json={
@@ -21,11 +23,10 @@ def envoyer_email_verification(utilisateur):
             "htmlContent": f"""<div style="font-family:Arial,sans-serif;max-width:600px;margin:0 auto;">
                 <div style="background:#1a1a2e;padding:30px;text-align:center;">
                     <h1 style="color:#f0a500;margin:0;">TontineSecure</h1>
-                    <p style="color:#fff;margin:5px 0;">Epargne collaborative securisee</p>
                 </div>
                 <div style="background:#fff;padding:40px;border:1px solid #eee;">
-                    <h2 style="color:#1a1a2e;">Bienvenue !</h2>
-                    <p style="color:#555;">Cliquez sur le bouton ci-dessous pour verifier votre email :</p>
+                    <h2>Bienvenue !</h2>
+                    <p>Cliquez ci-dessous pour verifier votre email :</p>
                     <div style="text-align:center;margin:30px 0;">
                         <a href="{lien}" style="background:#f0a500;color:#fff;padding:15px 30px;border-radius:10px;text-decoration:none;font-weight:bold;font-size:16px;">
                             Verifier mon email
